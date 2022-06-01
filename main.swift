046d6f6bb7adce832b1b94ff2089610270e5ed9a
Francisco Garache <franciscogarach3@gmail.com>
2022-06-01 22:48:27 +0000

    funciones

--- main.swift
+++ main.swift
@@ -2,31 +2,65 @@
 /*Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.*/
 
 
-var MATE: Int
-var ESPA: Int
-var CIEN: Int
-var HIST: Int
-var QUIM: Int
+var MATE = 0
+var ESPA = 0
+var CIEN = 0
+var HIST = 0
+var QUIM = 0
 var valor = "null"
+var ValidarNUM = false
 
-func notas (){
+func ImprimirNotas (){
+    print("")
     print(MATE, "-Matematicas")
     print(ESPA, "-Espaniol")
     print(CIEN, "-Ciencias")
-    print(HIST, "-H")
+    print(HIST, "-Historia")
 }
 
 
-print("Notas Semestrales \n")
+func pedirMate(){
+    print("\nIngrese su nota de matematicas ")
+    valor = readLine()!
+    MATE = Int(valor) ?? 0
+        if (MATE <= 100){
+            pedirEspa()
+        }else{
+            print("Nota Incorrecta Volver a Ingresar")
+    pedirMate()
+    }
+}
 
 
-print("\nIngrese su nota de matematicas ")
-valor = readLine()!
-MATE = Int(valor) ?? 0
-
+func pedirEspa(){
 print("\nIngrese su nota de Espaniol ")
 valor = readLine()!
 ESPA = Int(valor) ?? 0
+    if (ESPA<=100){
+        pedirCien()
+    }else{
+        print("Nota Incorrecta Volver a Ingresar")
+        pedirEspa()
+    }
+}
+
+func pedirCien (){
+    print("\nIngrese su nota de Ciencias ")
+    valor = readLine()!
+    CIEN = Int(valor) ?? 0
+        if(CIEN<=100){
+            ImprimirNotas()
+        }else{
+            print("Nota Incorrecta Volver a Ingresar")
+            pedirCien()
+        }
+}
+
+
+
+
+print("Notas Semestrales \n")
+pedirMate()
 
 
 
